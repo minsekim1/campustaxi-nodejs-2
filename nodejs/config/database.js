05dcedf7407d8397caf0e57f9a93c6946fb4b833
minsekim1 <tkarnrwl7862@naver.com>
2021-09-23 03:35:01 +0900

    원격서버연결

--- nodejs/config/database.js
+++ nodejs/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: "mysql",
         host: env("DATABASE_HOST", "3.35.144.255"),
         port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "campustaxi_2021_2"),
+        database: env("DATABASE_NAME", ""),
         username: env("DATABASE_USERNAME", "django"),
         password: env("DATABASE_PASSWORD", "m~WJ<iQs5DiSF*j#5yw/kA"),
       },
