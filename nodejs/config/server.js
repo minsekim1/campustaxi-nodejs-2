4c552c5358fe5e3008a83d0dc2f80003f6b274fe
Mins97 <tkarnrwl7862@naver.com>
2021-10-24 02:21:37 +0900

    port change 443

--- nodejs/config/server.js
+++ nodejs/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 443),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4ab1e009e9ae924e959705fc59c73bf4'),
