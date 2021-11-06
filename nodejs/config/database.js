91cd8bc7f99bed02c621dd0f91c544e2d634a073
Mins97 <tkarnrwl7862@naver.com>
2021-11-06 16:39:40 +0900

    username root

--- nodejs/config/database.js
+++ nodejs/config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "3.35.144.255"),
         port: env.int("DATABASE_PORT", 3306),
         database: env("DATABASE_NAME", "campustaxi_2021_2"),
-        username: env("DATABASE_USERNAME", "user"),
+        username: env("DATABASE_USERNAME", "root"),
         password: env("DATABASE_PASSWORD", "m~WJ<iQs5DiSF*j#5yw/kA"),
       },
       options: {},
