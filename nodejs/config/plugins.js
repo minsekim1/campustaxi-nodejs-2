310c3908055e7cae3bd869e887cbf79368d53a35
Mins97 <tkarnrwl7862@naver.com>
2021-11-13 11:15:18 +0900

    local image storage and AWS SQL

---
+++ nodejs/config/plugins.js
@@ -0,0 +1,14 @@
+
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "ftp-v2",
+    providerOptions: {
+      host: "218.153.157.69",
+      port: "21",
+      user: "minsekim",
+      password: "tkarnr78^@",
+      basePath: "/ftp/",
+      baseUrl: "http://218.153.157.69/ftp/",
+    },
+  },
+});
