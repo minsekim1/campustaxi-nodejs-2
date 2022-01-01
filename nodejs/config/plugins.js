8e9219f2d4ccdaf5fff70d394adf38be39f25a0b
Mins97 <tkarnrwl7862@naver.com>
2022-01-02 05:57:53 +0900

    ip changed

--- nodejs/config/plugins.js
+++ nodejs/config/plugins.js
@@ -3,13 +3,13 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "ftp-v2",
     providerOptions: {
-      host: "218.153.157.69",
+      host: "121.134.115.22",
       port: "21",
       user: "minsekim",
       password: "tkarnr78^@",
       basePath: "/ftp/",
-      baseUrl:
-        "http://218.153.157.69/ftp/",
+      baseUrl: "http://121.134.115.22/ftp/",
+      // "http://218.153.157.69/ftp/",
     },
   },
 });
